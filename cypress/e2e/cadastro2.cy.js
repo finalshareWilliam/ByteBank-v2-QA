c12be3872c058bcf7634701a19620e114fc4f35c
William Souza <william.h.souza@hotmail.com>
2023-10-12 00:37:56 -0300

    v1.4

---
+++ cypress/e2e/cadastro2.cy.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+describe('Desafio aula - Testa informações de um usuário específico', () => {
+    it('Verifica informações de usuário, como transações, saldo, nome, etc', () => {
+        cy.fixture('dadosUsuario').then((usuario) => {
+        cy.login(usuario.email, usuario.senha);
+        cy.visit('/home');
+        cy.url().should('include', '/home');
+  
+        cy.contains(usuario.nome).should('be.visible');
+        cy.getByData('lista-transacoes')
+          .find('li')
+          .last()
+          .contains(usuario.transacoes[usuario.transacoes.length - 1].valor);
+        cy.get('[data-testid="saldo"]').contains(usuario.saldo);
+      });
+    });
+  });
